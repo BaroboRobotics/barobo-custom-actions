7abb344a44bb057cbc253904edefffc81636469d
Harris Hancock <harris@barobo.com>
2016-03-23 11:53:17 -0700

    Add a simple install command

--- cxx_flag_overrides.cmake
+++ cxx_flag_overrides.cmake
@@ -1,4 +1,5 @@
 if(MSVC)
+    # Statically link with the CRT
     set(CMAKE_CXX_FLAGS_DEBUG_INIT "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
     set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
     set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
